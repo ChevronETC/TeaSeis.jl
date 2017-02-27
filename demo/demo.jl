8f098fc7e3c11527694399c4d04f1c371ad8e24a
samtkaplan <sam.t.kaplan@gmail.com>
2017-02-27 07:45:54 -0600

    simplify demo code and remove its dependence on PyPlot

---
+++ demo/demo.jl
@@ -0,0 +1,44 @@
+using TeaSeis
+
+# custom property with vector value:
+custompropdef = TracePropertyDef("SOU_X", "Source locations, x", Float64, 2)
+
+# create and open a new javaseis file:
+io = jsopen("test.js", "w", axis_lengths=[501,40,6], properties=[custompropdef])
+
+# create some data:
+data_write = rand(Float32, size(io))
+
+# write data (with headers) to the javaseis file:
+hdrs = allocframehdrs(io)
+for ifrm = 1:size(io,3)
+    map(i->set!(prop(io, stockprop[:TRACE]),    hdrs, i, i),                 1:size(io,2))
+    map(i->set!(prop(io, stockprop[:FRAME]),    hdrs, i, ifrm),              1:size(io,2))
+    map(i->set!(prop(io, stockprop[:TRC_TYPE]), hdrs, i, tracetype[:live]),  1:size(io,2))
+    map(i->set!(prop(io, custompropdef),        hdrs, i, i*ifrm*[1.0, 2.0]), 1:size(io,2))
+    writeframe(io, data_write[:,:,ifrm], hdrs)
+end
+
+# close the javaseis file:
+close(io)
+
+# open the dataset for reading:
+io = jsopen("test.js", "r")
+
+# allocate some memory for storing the read data:
+data_read = zeros(Float32, size(io))
+
+trcs, hdrs = allocframe(io)
+for ifrm = 1:size(io,3)
+    readframe!(io, trcs, hdrs, ifrm)
+    data_read[:,:,ifrm] = trcs
+    write(STDOUT, "read frame, trcs and hdrs API $(get(prop(io,stockprop[:FRAME]),hdrs[:,1]))\n")
+end
+
+# demonstrate that the extrema of the written and read data agree with each other:
+@show extrema(data_write)
+@show extrema(data_read)
+
+# delete the javaseis file:
+rm(io)
+
--- demo/demo_parallel.jl
+++ demo/demo_parallel.jl
@@ -3,23 +3,36 @@
 # this is just for illustration
 
 addprocs(2)
-using TeaSeis,PyPlot
+using TeaSeis
 
+# create and open a javaseis file for writing:
 io = jsopen("test.js", "w", axis_lengths=[501,40,6])
-data = rand(Float32, size(io))
 
+# create some data:
+data_write = rand(Float32, size(io))
+
+# parallel write data and headers to the javaseis file:
 hdrs = allocframehdrs(io)
 @sync @parallel for ifrm = 1:size(io,3)
     map(i->set!(prop(io, stockprop[:TRACE]),    hdrs, i, i),                1:size(io,2))
     map(i->set!(prop(io, stockprop[:FRAME]),    hdrs, i, ifrm),             1:size(io,2))
     map(i->set!(prop(io, stockprop[:TRC_TYPE]), hdrs, i, tracetype[:live]), 1:size(io,2))
-    writeframe(io, data[:,:,ifrm], hdrs)
+    writeframe(io, data_write[:,:,ifrm], hdrs)
 end
+
+# close the javaseis file:
 close(io)
 
-figure();imshow(data[:,:,3],cmap="seismic_r",clim=maxabs(data[:,:,3])*[-1,1],aspect="auto");title("writer_3d_parallel")
+# open the javaseis file for reading:
+io = jsopen("test.js", "r")
 
-if isinteractive() == false
-    PyPlot.show()
-    sleep(999999)
+# parallel read demonstration:
+@sync @parallel for ifrm = 1:size(io,3)
+    trcs, hdrs = readframe(io, ifrm)
+    write(STDOUT, "read frame $(get(prop(io,stockprop[:FRAME]),hdrs[:,1]))\n")
+    @show get(prop(io,stockprop[:FRAME]), hdrs[:,1]), extrema(trcs)
 end
+
+# delete javaseis file:
+rm(io)
+
